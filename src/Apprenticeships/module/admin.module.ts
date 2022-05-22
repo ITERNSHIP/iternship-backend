d20e578733a6f1f8c45dfba07375be6dd1e945d4
pornpipatpt <pornpipat.pt@mail.kmutt.ac.th>
2022-05-22 16:27:39 +0700

    [update]admin middleware

--- src/Apprenticeships/module/admin.module.ts
+++ src/Apprenticeships/module/admin.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminController } from '../controller/admin.controller';
 import { AdminEntity } from '../entities/admin.entity';
 import { CompanyEntity } from '../entities/company.entity';
 import { StaffEntity } from '../entities/staff.entity';
+import { AdminMiddleware } from '../middlewares/admin.middleware';
 import { AdminService } from '../service/admin.service';
 
 
@@ -15,4 +16,14 @@ import { AdminService } from '../service/admin.service';
   imports:[TypeOrmModule.forFeature([CompanyEntity,StaffEntity,AdminEntity])],
   exports:[AdminService]
 })
-export class AdminModule {}
+export class AdminModule implements NestModule {
+  public configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AdminMiddleware)
+      .exclude()
+      // .exclude({ path: 'users/login', method: RequestMethod.POST },
+      // { path: 'users/logout', method: RequestMethod.POST }
+      // )
+      // .forRoutes(AdminController);
+  }
+}
